f4daed159a03bf4041adcd1a80461b232c232398
Sam Narisi <snarisi@gmail.com>
2015-12-10 09:51:14 -0500

    Create states and routes for builings

--- browser/js/new-building/new-building.controller.js
+++ browser/js/new-building/new-building.controller.js
@@ -1,3 +1,11 @@
 app.controller('NewBuildingCtrl', function ($scope) {
-	
+	$scope.newBuilding = {
+		name: $scope.buildingName,
+		location: $scope.location,
+		architect: $scope.architect,
+		style: $scope.style,
+		image: $scope.image,
+		description: $scope.description
+	}
+
 });
